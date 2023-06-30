f446f6fac381e10a5172dfb33210d05f9b177194
Alexander Gromnitsky <alexander.gromnitsky@gmail.com>
2023-06-30 23:44:03 +0300

    add a simple server skelethon

--- gtk4_shadertoy.c
+++ gtk4_shadertoy.c
@@ -13,7 +13,71 @@ typedef struct {
   gboolean fullscreen;
   gboolean fps;
   gboolean below;
+  gint port;
 } Opt;
+
+gboolean on_socket_msg(GThreadedSocketService *service,
+                       GSocketConnection *conn,
+                       GSocketListener *listener, gpointer user_data) {
+  GOutputStream *out = g_io_stream_get_output_stream(G_IO_STREAM(conn));
+  GInputStream *in = g_io_stream_get_input_stream (G_IO_STREAM(conn));
+  char buf[BUFSIZ];
+  gssize size;
+
+  char *hello = "Available commands: pause, load file.glsl, quit\n";
+  g_output_stream_write_all(out, hello, strlen(hello), NULL, NULL, NULL);
+
+  while (0 < (size = g_input_stream_read(in, buf, sizeof buf, NULL, NULL))) {
+    char *res = "400 invalid command\n";
+    buf[size] = '\0';
+    char *req = g_strstrip(buf);
+
+    GRegex *re = g_regex_new("\\s+", G_REGEX_DEFAULT,
+                             G_REGEX_MATCH_DEFAULT, NULL);
+    char **cmd = g_regex_split(re, req, G_REGEX_MATCH_DEFAULT);
+
+    if (1 == g_strv_length(cmd)) {
+      if (0 == strcmp(req, "pause")) {
+        res = "500 pause\n";
+        //shader_pause(opt->shader);
+      }
+      if (0 == strcmp(req, "quit")) exit(0);
+    }
+
+    if (2 == g_strv_length(cmd)) {
+      if (0 == strcmp(cmd[0], "load")) {
+        res = "200 load\n";
+      }
+    }
+
+    g_regex_unref(re);
+    g_strfreev(cmd);
+
+    g_output_stream_write(out, res, strlen(res), NULL, NULL);
+  }
+
+  return TRUE;
+}
+
+gboolean socket_listen(Opt *opt) {
+  GError *error = NULL;
+  GSocketService *service = g_threaded_socket_service_new(10);
+  GInetAddress *inet_addr = g_inet_address_new_from_string("127.0.0.1");
+  GSocketAddress *addr = g_inet_socket_address_new(inet_addr, opt->port);
+  gboolean ok = g_socket_listener_add_address(G_SOCKET_LISTENER(service),
+                                              addr, G_SOCKET_TYPE_STREAM,
+                                              G_SOCKET_PROTOCOL_TCP,
+                                              NULL, NULL, &error);
+  g_object_unref(inet_addr);
+  g_object_unref(addr);
+  if (!ok) {
+    g_log(NULL, G_LOG_LEVEL_CRITICAL, "%s", error->message);
+    return FALSE;
+  }
+  g_message("Listening on localhost:%d", opt->port);
+  g_signal_connect(service, "run", G_CALLBACK(on_socket_msg), opt);
+  return TRUE;
+}
 
 void fullscreen_toggle(GtkWidget *win) {
   void (*fn)(GtkWindow*) = gtk_window_is_fullscreen(GTK_WINDOW(win)) ? gtk_window_unfullscreen : gtk_window_fullscreen;
@@ -67,6 +131,11 @@ gchar* input(char *file) {
 }
 
 void app_activate(GApplication *app, Opt *opt) {
+  if (opt->port && !socket_listen(opt)) {
+    // how do I provide status for g_application_quit()?
+    exit(1);
+  }
+
   if (!opt->shader_src) opt->shader_src = input(NULL); // try to read stdin
   if (opt->below) g_set_prgname("gtk4_shadertoy_below");
 
@@ -143,6 +212,7 @@ int main(int argc, char **argv) {
     { "fullscreen", 'f', 0,G_OPTION_ARG_NONE,&opt.fullscreen,NULL,NULL },
     { "below", 'b', 0,G_OPTION_ARG_NONE, &opt.below,"fake x11 root window",NULL},
     { "fps", 'r', 0,G_OPTION_ARG_NONE,&opt.fps,"show an FPS overlay", NULL },
+    { "port", 'p', 0,G_OPTION_ARG_INT,&opt.port,"start a server", "integer" },
     { NULL }
   };
   g_application_add_main_option_entries(G_APPLICATION(app), params);
